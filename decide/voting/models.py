01207474eca7ebd2ba48e5c4adba2fc0cd37a96f
anacatcam <accamaxo.10@gmail.com>
2020-12-19 10:15:59 +0000

    Implementación métodos auxiliares de votaciones

--- decide/voting/models.py
+++ decide/voting/models.py
@@ -35,6 +35,16 @@ class Votacion(models.Model):
     def __str__(self):
         return self.titulo
 
+ #DEVUELVE EL NÚMERO DE PREGUNTAS QUE TIENE ASOCIDADA UNA VOTACION  
+    def Numero_De_Preguntas(self):
+        return Pregunta.objects.filter(votacion_id=self.id).count()
+   #AÑADE UNA PREGUNTA  A LA VOTACION 
+   #A LA HORA DE CREAR LA PREGUNTA SOLO ES NECESARIO INDICARLE EL ATRIBUTO TEXTOPREGUNTA
+   # LA FUNCION SE ENCARGA DE ASOCIAR LA PREGUNTA  A LA VOTACION QUE SE LE HA INDICADO  
+    def addPregunta(self,pregunta):
+        pregunta.votacion = self
+        pregunta.save()
+
 #MODELO DE PREGUNTAS
 class Pregunta(models.Model):
     id = models.AutoField(primary_key=True)
@@ -44,6 +54,41 @@ class Pregunta(models.Model):
         return self.votacion.titulo
     def __str__(self):
         return self.textoPregunta
+        
+#DEVUELVE EL NÚMERO DE VECES QUE SE HA RESPONDIDO A LA PREGUNTA
+   #COINCIDE CON EL NÚMERO DE VECES QUE SE HA RESPONDIDO A LA VOTACIÓN ASOCIDADA A LA PREGUNTA
+   #YA QUE EN UNA VOTACIÓN SE TIENEN QUE CONTESTAR A TODAS LAS PREGUNTAS  
+    def Numero_De_Respuestas(self):
+        return Respuesta.objects.filter(pregunta_id=self.id).count()
+
+   #DEVUELVE LA CALIFICACIÓN MEDIA DE LA PREGUNTA EN BASE A LAS RESPUESTAS DADAS 
+    def Media_De_Las_Respuestas(self):
+        respuestas = Respuesta.objects.filter(pregunta_id=self.id).values('respuesta')
+        n_respuestas = len(respuestas)
+        if n_respuestas == 0:##EVITAR DIVISIÓN POR CERO 
+            n_respuestas=1
+        total = 0
+        for value in respuestas:
+            total = total + value['respuesta']
+        return total/n_respuestas
+    
+   #DEVUELVE LA RESPUESTA MAS ALTA DADA A UNA PREGUNTA 
+    def Respuesta_Maxima(self):
+        max = Respuesta.objects.filter(pregunta_id=self.id).aggregate(Max('respuesta'))
+        return str(max['respuesta__max'])
+
+    #DEVUELVE LA RESPUESTA MAS BAJA DADA A UNA PREGUNTA 
+    def Respuesta_Minima(self):
+        min = Respuesta.objects.filter(pregunta_id=self.id).aggregate(Min('respuesta'))
+        return str(min['respuesta__min'])
+
+
+    #AÑADE UNA RESPUESTA  A LA PREGUNTA 
+   #A LA HORA DE CREAR LA RESPUESTA SOLO ES NECESARIO INDICARLE EL ATRIBUTO RESPUESTA
+   # LA FUNCION SE ENCARGA DE ASOCIAR LA RESPUESTA  A LA PREGUNTA QUE SE LE HA INDICADO
+    def addRespuesta(self,respuesta):
+        respuesta.pregunta = self
+        respuesta.save()
 
 #Modelo de Respuesta
 class Respuesta(models.Model):
