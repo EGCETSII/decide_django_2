23f25ba41d6740ee10fba2b03c8e4b3399777ad5
nicpazsar <nicpazsar@alum.us.es>
2020-12-27 18:03:17 +0100

    Estilo añadido para voting

--- decide/booth/decide-react/src/components/Voting.js
+++ decide/booth/decide-react/src/components/Voting.js
@@ -4,6 +4,8 @@ import { ElGamal } from '../crypto/ElGamal';
 import { Alert, Button, Picker, Text, View } from 'react-native';
 import { postData } from '../utils';
 import config from '../config.json';
+import { StyleSheet} from "react-native";
+
 
 export default class Voting extends Component {
 
@@ -55,16 +57,83 @@ export default class Voting extends Component {
     render() {
         const { voting, resetSelected } = this.props;
 
-        return <View>
-            <Text style={{fontSize: 15}}>{voting.name}</Text>
-            <Text style={{fontSize: 13}}>{voting.question.desc}</Text>
-            <Picker selectedValue={this.state.selected} onValueChange={(itemValue, itemIndex) => this.setState({selected: itemValue})}>
-                {voting.question.options.map(opt => 
-                    <Picker.Item label={opt.option} value={opt.number} key={opt.number} />
-                )}
-            </Picker>
-            <Button title="Votar" onPress={this.handleSubmit} />
-            <Button title="Volver" color="#333" onPress={resetSelected} />
-        </View>;
+        return <View style={styles.html}>
+                <View style={styles.container}>
+                    <View style={styles.content}>
+                        <View style={styles.row}>
+                            <View style={styles.clearfix}>
+                                <Text style={styles.textStyle}>{voting.name}</Text>
+                                <Text style={styles.textStyle}>{voting.question.desc}</Text>
+                            </View>
+                            <View style={styles.clearfix}>
+                                <Picker style={styles.pickerStyle} selectedValue={this.state.selected} onValueChange={(itemValue, itemIndex) => this.setState({selected: itemValue})}>
+                                    {voting.question.options.map(opt => 
+                                        <Picker.Item label={opt.option} value={opt.number} key={opt.number} />
+                                    )}
+                                </Picker>
+                            </View>
+                            <View style={styles.clearfix}> 
+                                <Button title="Votar" color="blue" onPress={this.handleSubmit} />
+                            </View>
+                            <View style={styles.clearfix}> 
+                                <Button title="Volver" color="black" onPress={resetSelected} />
+                            </View>
+                        </View>    
+                    </View>
+                </View>
+        </View>
     }
 }
+
+const styles = StyleSheet.create ({
+    htmlStyle: {
+        marginTop: 0,
+        marginBottom: 0,
+        marginRight: 0,
+        marginLeft: 0,
+        paddingTop: 0,
+        paddingBottom: 0,
+        paddingRight: 0,
+        paddingLeft: 0
+    },
+
+    containerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    contentStyle: {
+        width: 960,
+        backgroundColor: 'fff',
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        borderBottomRightRadius: 10,
+        borderBottomLeftRadius: 10,
+        overflow: 'hidden',
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+        paddingTop: 50,
+        paddingRight: 50,
+        paddingBottom: 50,
+        paddingLeft: 50
+    },
+    textStyle:{
+        fontSize: 18,
+        lineHeight: 24,
+        justifyContent: 'center',
+        alignSelf: 'center'
+    },
+    pickerStyle:{  
+        height: 40,  
+        width: 800,  
+        color: 'rgb(7, 7, 76)',  
+        justifyContent: 'center',
+        alignSelf: 'center'  
+    },
+    row: {
+    },
+    clearfix: {
+        "marginBottom": 24,
+        "zoom": 1
+    }
+});
