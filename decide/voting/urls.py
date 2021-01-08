def141238a4c3df992c0f648e9330c5f69e789e4
fraarrapo <fraarrapo@alum.us.es>
2021-01-08 13:29:37 +0100

    Arreglo hotfix/VO-038
    Se han implementado las vistas y la conversion Json

--- decide/voting/urls.py
+++ decide/voting/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    # path('', views.VotingView.as_view(), name='voting'),
-    # path('<int:voting_id>/', views.VotingUpdate.as_view(), name='voting'),
+    path('', views.VotingView.as_view(), name='voting'),
+    path('<int:voting_id>/', views.VotingUpdate.as_view(), name='voting'),
+    path('votacionBinaria/<id>/', views.getVotacionBinariaById, name='Votacion Binaria'),
+    path('votacion/<id>/', views.getVotacionById, name='Votacion'),
+    path('votacionMultiple/<id>/', views.getVotacionMultipleById, name='Votacion Multiple'),
+    path('votacionPreferencia/<id>/', views.getVotacionPreferenciaById, name='Votacion Preferencia')
 ]
