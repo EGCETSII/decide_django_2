f4989316dff43c9c4530d6e62a7e76339e0e6d31
D-RHYM <davcacrom@alum.us.es>
2021-01-12 12:29:36 +0100

    Feature: Añadir vista login; #16
    Se suben los archivos urls.py y views.py que faltaban por corregir.

--- decide/booth/urls.py
+++ decide/booth/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import BoothView
+from . import views
 
 
 urlpatterns = [
     path('<int:voting_id>/', BoothView.as_view()),
+    path('login/', views.loginPage, name="login"),
+    path('', views.welcome, name="welcome"),
 ]
