a6409506e2f7663e2594a5215881f5d816758980
lauporsol <lauraportillosoler@gmail.com>
2021-01-15 21:00:42 +0100

    Intento de implementación de Json manual

--- decide/booth/views.py
+++ decide/booth/views.py
@@ -18,6 +18,7 @@ from django.contrib.auth.decorators import login_required
 
 from .forms import CrearUsuario
 from voting.views import VotingView, VotingUpdate
+from voting.models import Voting, Question, PoliticalParty
 
 # Create your views here.
 
@@ -25,11 +26,33 @@ from voting.views import VotingView, VotingUpdate
 # TODO: check permissions and census
 class BoothView(TemplateView):
     template_name = 'booth/booth.html'
+    '''q = Question.objects.create(desc="¿Esto es un ejemplo?")
+    p = PoliticalParty.objects.create(name="Political23313", acronym="P223133", description="Esto es una descripción", leader="Líder2", president="Presidente2")
+    v = Voting.objects.create(name="Votación 1", desc="Esto es un ejemplo", question=q, political_party=p, start_date="2021-01-12 00:00", end_date="2021-01-30 00:00", url="http://localhost:8000/booth/")'''
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+        
+        x = {
+            "voting_id": 4,
+            "name": "Votacion 1",
+            "desc": "Esto es un ejemplo",
+            "question": {
+                "desc": "¿Esto es un ejemplo?"},
+            "political_party": {
+                "name":"Fiesta politica",
+                "acronym": "FP",
+                "description": "Esto es una fiesta politica",
+                "leader": "Líder de la fiesta",
+                "predident": "Presidente de la fiesta"},
+            "start-date":"2021-01-12 00:00",
+            "end-date":"2021-01-30 00:00",
+            "url":"http://localhost:8000/booth/4",
+            "pub-key": "a1s2d3f4g5h6j7k8l9"
+            }
+        
+        '''context = super().get_context_data(**kwargs)
         vid = kwargs.get('voting_id', 0)
-
+        print(vid)
         try:
             r = mods.get('voting', params={'id': vid})
 
@@ -37,26 +60,18 @@ class BoothView(TemplateView):
             # and avoid problems with js and big number conversion
             for k, v in r[0]['pub_key'].items():
                 r[0]['pub_key'][k] = str(v)
+                print(str(v))
 
             context['voting'] = json.dumps(r[0])
         except:
             raise Http404
 
-        context['KEYBITS'] = settings.KEYBITS
-        context['start_date'] = self.get_format_date(context['voting']['start_date'])
-        context['end_date'] = self.get_format_date(context['voting']['end_date'])
-
-        return context
-    
-    def get_format_date(self, fecha):
-        result = None
-
-        if fecha != None:
-            fecha = fecha.replace("T", " ").replace("Z", "")
-            date_time = datetime.datetime.strptime(fecha, '%Y-%m-%d %H:%M:%S.%f')
-            result = date_time.strftime('%d/%m/%Y a las %H:%M:%S')
-
-        return result 
+        context['KEYBITS'] = settings.KEYBITS'''
+        x['KEYBITS'] = settings.KEYBITS
+        print(x)
+        y = json.dumps(x)
+        print(y)
+        return y
 
     
 def prueba(request):
