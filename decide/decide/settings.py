c12d12a46e0e116eb64885053659727e4a8cd483
Fernando Rabasco Ledesma <48551658+ferrabled@users.noreply.github.com>
2021-12-01 15:58:48 +0100

    Changed URL for heroku

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -70,7 +70,7 @@ MODULES = [
     'voting',
 ]
 
-BASEURL = 'http://localhost:8000'
+BASEURL = 'https://egc-decide-fr.herokuapp.com'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
