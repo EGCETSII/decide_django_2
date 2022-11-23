55fc2e347ce7f6d1d292577389f4812a04085de3
jvegax <toledo.utrera@hotmail.com>
2022-11-23 21:14:40 +0100

    chore: fix pycrypto versions

--- decide/build.sh
+++ decide/build.sh
@@ -2,5 +2,9 @@
 # exit on error
 set -o errexit
 
+pip uninstall PyCrypto
+pip uninstall PyCryptodome
+pip install PyCryptodome
+
 python manage.py collectstatic --no-input
 python manage.py migrate
