bb4644c32d381eeef40d755b1d983c58d4d39370
jvegax <toledo.utrera@hotmail.com>
2022-11-23 21:49:15 +0100

    chore: idk

--- decide/build.sh
+++ decide/build.sh
@@ -2,9 +2,5 @@
 # exit on error
 set -o errexit
 
-pip uninstall PyCrypto
-pip uninstall PyCryptodome
-pip install PyCryptodome
-
 python manage.py collectstatic --no-input
 python manage.py migrate
