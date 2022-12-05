b75819134d3eb1264e6a290427cdfcbbe6cbe5c5
Diego <diewentdiaz@gmail.com>
2022-12-05 21:56:52 +0100

    fix: Tokens en swagger

--- decide/voting/serializers.py
+++ decide/voting/serializers.py
@@ -31,7 +31,9 @@ class VotingSerializer(serializers.HyperlinkedModelSerializer):
 
 class SimpleVotingSerializer(serializers.HyperlinkedModelSerializer):
     question_opt = QuestionSerializer(many=False)
+    token = serializers.CharField()
+    # token = serializers.SerializerMethodField('token')
     class Meta:
         model = Voting
-        fields = ('name', 'desc', 'question', 'question_opt')
+        fields = ('name', 'desc', 'question', 'question_opt', 'token')
 
